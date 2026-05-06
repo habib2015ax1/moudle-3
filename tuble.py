5c6cfb15d9554088f073900232bbe6a8397e436b
habib2015ax <Habib1@#$>
2026-05-06 11:37:27 +0400

    cccccccc

---
+++ tuble.py
@@ -0,0 +1,14 @@
+tuple2=(222,333,444,555,666,777,888,999)
+tuple1=(11,11,11,11,11,11,11,11,11,11,11,11,11,11,22,33,44,55,66,77,88,99,111)
+tuple=(len(tuple1))
+print(tuple1)
+for item in tuple1:
+    print(item)
+print (tuple1[1:2])
+print(max (tuple1))
+print (min (tuple1))
+print(tuple1+tuple2)
+print (sorted (tuple1))
+print (tuple1.count(11))
+
+
---
+++ weather prediction.py
@@ -0,0 +1,7 @@
+tuple=(34,34,33,33,33,33,32,31)
+avarage=sum(tuple)/len(tuple)
+print(avarage)
+if avarage>=32:
+    print("prediction weather will be hot")
+else:
+    print("weather will be normal")
