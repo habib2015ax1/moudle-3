6e44cd8cc71f6e20dbda23688a8d0b0dffb28d53
habib2015ax <Habib1@#$>
2026-05-10 11:22:31 +0400

    iphone

---
+++ sutdata.py
@@ -0,0 +1,11 @@
+student1={"Name":'Mohammed',"Grade":"5","City":"UAE"}
+print(student1)
+
+student1["phone number"]="97174668569857"
+
+print(student1["Name"])
+
+print(student1.get("phone number"))
+
+print(student1.pop("Grade"))
+print(student1)
