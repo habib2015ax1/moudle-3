f244f87ce6faa3d996ac804cf0291e0a74c481aa
habib2015ax <Habib1@#$>
2026-05-17 11:26:32 +0400

    ggggg

---
+++ set,py.py
@@ -0,0 +1,15 @@
+cricket={"Mohammed","Zaeem","Farhan"}
+
+basketball={"Mohammed","Riddhi","reeva","Riddhan"}
+
+#operations on set
+
+print(cricket.union(basketball))
+
+print(cricket.intersection(basketball))
+
+print(cricket.difference(basketball))
+
+print(basketball.difference(cricket))
+
+print(cricket.symmetric_difference(basketball))
