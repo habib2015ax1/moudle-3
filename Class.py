1091f35aac3205616b3e13e64bafe382d923d92e
habib2015ax <Habib1@#$>
2026-06-13 11:46:23 +0400

    submit

---
+++ Class.py
@@ -0,0 +1,8 @@
+class words:
+        def __init__(self):
+           str=""
+        def input (self):
+              self.str=input("enter string")
+              print (self.str.upper())
+word= words()
+word.input()   
