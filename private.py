c94a9c38a17ce1c7aa7641fc2ceb0cda727616dd
habib2015ax <Habib1@#$>
2026-07-11 11:04:25 +0530

    upload

---
+++ private.py
@@ -0,0 +1,14 @@
+class myClass:
+    _privateVar = 27
+ 
+
+    def _privateMeth(self):
+      print("I,m inside my class myClass")
+
+    def hello(self):
+      print ("private Variable value:",myClass.__privateVar)
+
+foo = myClass()
+foo.hello()
+foo.__privMeth()
+p
